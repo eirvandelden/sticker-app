891fbc2e8cae071eb5ca813d92aa4667249c1dbd
Etienne van Delden de la Haije <etienne@vandelden.family>
2025-08-05 00:10:31 +0200

    Add basic rails8 secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  enum :role, { child: 0, parent: 1 }
+
+  has_one :child_profile, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
 end
