cabb3ca1f9cca7f41b449b7cd3fc9db95ef8d0b5
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-07-01 14:50:19 +0200

    Label flash notification sections

---
+++ test/integration/flash_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class FlashTest < ActionDispatch::IntegrationTest
+  test "application flash container is labelled" do
+    sign_in_as(users(:user))
+
+    patch preferences_path, params: { user: { locale: "en" } }
+    follow_redirect!
+
+    assert_select %(section[data-mvpa-flashes][aria-label="#{I18n.t("flash.notifications")}"])
+  end
+
+  test "admin flash container is labelled" do
+    admin = users(:admin)
+    sign_in_as(admin)
+
+    delete admin_user_path(admin)
+    follow_redirect!
+
+    assert_select %(section[data-mvpa-flashes][aria-label="#{I18n.t("flash.notifications")}"])
+  end
+end
