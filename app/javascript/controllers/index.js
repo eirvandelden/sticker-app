a2e6118943fc3c0b65c21b26120c036d88fc346a
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-07-19 12:02:26 +0200

    fix: apply saved theme before Turbo renders

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,8 +6,5 @@ eagerLoadControllersFrom("controllers", application)
 import PushController from "appkit/controllers/push_controller"
 application.register("push", PushController)
 
-import ThemeController from "appkit/controllers/theme_controller"
-application.register("theme", ThemeController)
-
 import AutoSubmitController from "appkit/controllers/auto_submit_controller"
 application.register("auto-submit", AutoSubmitController)
