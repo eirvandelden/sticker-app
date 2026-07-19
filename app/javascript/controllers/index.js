9ef90928f127748c858d5e4ed635f1897d11b9b9
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-07-19 12:04:33 +0200

    fix: register host theme controller explicitly

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,6 +3,9 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+import ThemeController from "controllers/theme_controller"
+application.register("theme", ThemeController)
+
 import PushController from "appkit/controllers/push_controller"
 application.register("push", PushController)
 
